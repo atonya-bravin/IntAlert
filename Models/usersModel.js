b35f5e5dba799ee9715f1fbe9a946207f8026a99
laban254 <labanrotich6544@gmail.com>
2023-09-16 09:30:50 +0300

    user models

---
+++ Models/usersModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema;
+const {v4, uuidv4} = require('uuidv4');
+
+const userSchema = new schema({
+    
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+const user = mongoose.model('User', userSchema);
+
+module.exports = user;
