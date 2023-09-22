82060012b85bf53837cf4cf75d4114043628d5d0
atonya-bravin <bravinatonya.brani@gmail.com>
2023-09-22 17:30:46 +0300

    in local

--- public/scripts/complains_bay.js
+++
@@ -1,8 +0,0 @@
-const confirmLinkButton = document.getElementById("confirm_button");
-
-confirmLinkButton.addEventListener("click", () => {
-    const linkTextBox = document.getElementById("video_link");
-    const link = linkTextBox.value;
-    window.open(link,"_blank");
-});
-
